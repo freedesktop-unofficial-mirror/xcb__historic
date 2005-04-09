955996f8edb3996aeece4cde794714f9924f6a08
Jamey Sharp <jamey@minilop.net>
2005-04-09 23:39:37 +0000

    This is an XCB app. Why not... I don't know... hide latency?

--- xcb-demo/dpms.c
+++ xcb-demo/dpms.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001-2002 Bart Massey and Jamey Sharp.
+ * Copyright (C) 2001-2005 Bart Massey and Jamey Sharp.
  * All Rights Reserved.  See the file COPYING in this directory
  * for licensing information.
  */
@@ -10,48 +10,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void check_dpms_version(XCBConnection *);
-void check_dpms_capable(XCBConnection *);
-void print_dpms_timeouts(XCBConnection *);
-
 int main(int argc, char **argv)
 {
 	XCBConnection *c = XCBConnectBasic();
-	/*XCB_DPMS_Init(c);*/
-	check_dpms_version(c);
-	check_dpms_capable(c);
+	XCBDPMSGetVersionCookie vc;
+	XCBDPMSGetVersionRep *ver;
+	XCBDPMSCapableCookie cc;
+	XCBDPMSCapableRep *cap;
+	XCBDPMSGetTimeoutsCookie tc;
+	XCBDPMSGetTimeoutsRep *time;
 
-	print_dpms_timeouts(c);
+	XCBPrefetchExtensionData(c, &XCBDPMSId);
 
-	exit(0);
-	/*NOTREACHED*/
-}
+	vc = XCBDPMSGetVersion(c, 1, 1);
+	cc = XCBDPMSCapable(c);
+	tc = XCBDPMSGetTimeouts(c);
 
-void check_dpms_version(XCBConnection *c)
-{
-	XCBDPMSGetVersionCookie cookie = XCBDPMSGetVersion(c, 1, 1);
-	XCBDPMSGetVersionRep *ver = XCBDPMSGetVersionReply(c, cookie, 0);
+	ver = XCBDPMSGetVersionReply(c, vc, 0);
+	cap = XCBDPMSCapableReply(c, cc, 0);
+	time = XCBDPMSGetTimeoutsReply(c, tc, 0);
+
 	assert(ver);
 	assert(ver->server_major_version == 1);
 	assert(ver->server_minor_version == 1);
 	free(ver);
-}
 
-void check_dpms_capable(XCBConnection *c)
-{
-	XCBDPMSCapableCookie cookie = XCBDPMSCapable(c);
-	XCBDPMSCapableRep *cap = XCBDPMSCapableReply(c, cookie, 0);
 	assert(cap);
 	assert(cap->capable);
 	free(cap);
-}
 
-void print_dpms_timeouts(XCBConnection *c)
-{
-	XCBDPMSGetTimeoutsCookie cookie = XCBDPMSGetTimeouts(c);
-	XCBDPMSGetTimeoutsRep *time = XCBDPMSGetTimeoutsReply(c, cookie, 0);
 	assert(time);
 	printf("Standby: %d\n" "Suspend: %d\n" "Off: %d\n",
 		time->standby_timeout, time->suspend_timeout, time->off_timeout);
 	free(time);
+
+	XCBDisconnect(c);
+
+	exit(0);
+	/*NOTREACHED*/
 }
