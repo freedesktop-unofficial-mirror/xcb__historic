c0779d99e8568c90cc6dfd9237133c8c9aca0e9b
Jamey Sharp <jamey@minilop.net>
2005-04-10 01:03:31 +0000

    Implement XCBPrefetchExtensionData. An extension can now be in one of three     states in a particular connection's cache: not queried; query cookie     available; and query reply available.

--- xcb/src/xcb_ext.c
+++ xcb/src/xcb_ext.c
@@ -32,38 +32,65 @@
 #include "xcbext.h"
 #include "xcbint.h"
 
-/* Public interface */
+typedef struct {
+    enum { LAZY_COOKIE, LAZY_FORCED } tag;
+    union {
+        XCBQueryExtensionCookie cookie;
+        XCBQueryExtensionRep *reply;
+    } value;
+} lazyreply;
 
-/* Do not free the returned XCBQueryExtensionRep - on return, it's aliased
- * from the cache. */
-const XCBQueryExtensionRep *XCBGetExtensionData(XCBConnection *c, XCBExtension *ext)
+static lazyreply *get_lazyreply(XCBConnection *c, XCBExtension *ext)
 {
     static pthread_mutex_t global_lock = PTHREAD_MUTEX_INITIALIZER;
     static int next_global_id;
 
-    XCBQueryExtensionRep *data;
+    lazyreply *data;
 
     pthread_mutex_lock(&global_lock);
     if(!ext->global_id)
         ext->global_id = ++next_global_id;
     pthread_mutex_unlock(&global_lock);
 
-    pthread_mutex_lock(&c->ext.lock);
     data = _xcb_map_get(c->ext.extensions, ext->global_id);
     if(!data)
     {
         /* cache miss: query the server */
-        data = XCBQueryExtensionReply(c, XCBQueryExtension(c, strlen(ext->name), ext->name), 0);
+        data = malloc(sizeof(lazyreply));
+        if(!data)
+            return 0;
+        data->tag = LAZY_COOKIE;
+        data->value.cookie = XCBQueryExtension(c, strlen(ext->name), ext->name);
         _xcb_map_put(c->ext.extensions, ext->global_id, data);
+    }
+    return data;
+}
+
+/* Public interface */
+
+/* Do not free the returned XCBQueryExtensionRep - on return, it's aliased
+ * from the cache. */
+const XCBQueryExtensionRep *XCBGetExtensionData(XCBConnection *c, XCBExtension *ext)
+{
+    lazyreply *data;
+
+    pthread_mutex_lock(&c->ext.lock);
+    data = get_lazyreply(c, ext);
+    if(data && data->tag == LAZY_COOKIE)
+    {
+        data->tag = LAZY_FORCED;
+        data->value.reply = XCBQueryExtensionReply(c, data->value.cookie, 0);
     }
     pthread_mutex_unlock(&c->ext.lock);
 
-    return data;
+    return data ? data->value.reply : 0;
 }
 
 void XCBPrefetchExtensionData(XCBConnection *c, XCBExtension *ext)
 {
-    /* XXX: implement me, I'm an optimization */
+    pthread_mutex_lock(&c->ext.lock);
+    get_lazyreply(c, ext);
+    pthread_mutex_unlock(&c->ext.lock);
 }
 
 /* Private interface */
