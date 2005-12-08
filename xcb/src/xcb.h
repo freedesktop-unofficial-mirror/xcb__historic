25b4477fe5e84887896821af036891edcef382d4
Jamey Sharp <jamey@minilop.net>
2005-12-08 07:15:12 +0000

    Looks like the deprecated existed back in GCC 3.1.

--- xcb/src/xcb.h
+++ xcb/src/xcb.h
@@ -36,7 +36,7 @@
 extern "C" {
 #endif
 
-#if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3))
+#if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
 #define deprecated __attribute__((__deprecated__))
 #else
 #define deprecated
