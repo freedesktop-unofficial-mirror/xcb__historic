6b44b443af875cf174d9befc0a735a35315ae211
Jamey Sharp <jamey@minilop.net>
2006-01-11 05:32:47 +0000

    Add support for requests with multiple replies, and somewhat generic reply     parsing. Implement ListFontsWithInfo on the result.

--- xhsb/XProto.glue.c
+++ xhsb/XProto.glue.c
@@ -5,3 +5,9 @@ CARD32 _internAtom(XCBConnection *c, BOOL onlyIfExists, CARD16 name_len, char *n
 	XCBInternAtomCookie cookie = XCBInternAtom(c, onlyIfExists, name_len, name);
 	return cookie.sequence;
 }
+
+CARD32 _ListFontsWithInfo(XCBConnection *c, CARD16 max_names, CARD16 pattern_len, char *pattern)
+{
+	XCBListFontsWithInfoCookie cookie = XCBListFontsWithInfo(c, max_names, pattern_len, pattern);
+	return cookie.sequence;
+}
